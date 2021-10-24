c0ec349482c5ec52b84c014928fb1c826930ded7
rsfbernardes <rsfbernardes@hotmail.com>
2021-10-24 15:22:49 -0300

    implemented URL class and request.query

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const http = require('http');
 const routes = require('./routes');
+const { URL } = require('url');
 
 const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  console.log(parsedUrl);
 
   const route = routes.find((routeObj) => (
     routeObj.method === request.method && routeObj.endpoint === request.url
   ));
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);  
   }
   else {
