70dab4b88ee63f3d64762fff9aa7bcca47f208e9
rsfbernardes <rsfbernardes@hotmail.com>
2021-10-24 15:16:45 -0300

    implemented route and handler on index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('Hello world');
+  const route = routes.find((routeObj) => (
+    routeObj.method === request.method && routeObj.endpoint === request.url
+  ));
+
+  if (route) {
+    route.handler(request, response);  
+  }
+  else {
+    response.writeHead(400, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
 });
 
 server.listen(3000, () => console.log('🔛 Server started at http://localhost:3000'));
